951b3ebc7f20bccaf0d540d5275c18e7e560b55c
jshmslf <jshmslf@gmail.com>
2026-02-11 15:24:45 +0800

    changed layout and added new photos

--- server/assets-chunks/index_server_html.mjs
+++ server/assets-chunks/index_server_html.mjs
@@ -7,10 +7,10 @@ export default `<!doctype html>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="/feather-dynamics-box.png">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"/>
-<link rel="stylesheet" href="styles-P2TCDWBA.css"></head>
+<link rel="stylesheet" href="styles-6XLD3XM7.css"></head>
 <body><script type="text/javascript" id="ng-event-dispatch-contract">(()=>{function p(t,n,r,o,e,i,f,m){return{eventType:t,event:n,targetElement:r,eic:o,timeStamp:e,eia:i,eirp:f,eiack:m}}function u(t){let n=[],r=e=>{n.push(e)};return{c:t,q:n,et:[],etc:[],d:r,h:e=>{r(p(e.type,e,e.target,t,Date.now()))}}}function s(t,n,r){for(let o=0;o<n.length;o++){let e=n[o];(r?t.etc:t.et).push(e),t.c.addEventListener(e,t.h,r)}}function c(t,n,r,o,e=window){let i=u(t);e._ejsas||(e._ejsas={}),e._ejsas[n]=i,s(i,r),s(i,o,!0)}window.__jsaction_bootstrap=c;})();
 </script>
   <app-root></app-root>
-<script src="main-6A3RRPAX.js" type="module"></script></body>
+<script src="main-6QGEYWOM.js" type="module"></script></body>
 </html>
 `;
